dba3e37570c4306102ebe02ce9607a3dcbb8ed40
pa-dev <coderhookdev@gmail.com>
2025-01-03 14:20:50 +0100

    clean about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,6 @@
-import { GetStaticProps } from 'next';
 import { getMarkdownContent } from '@/lib/markdown';
 import MarkdownWrapper from '@/components/markdown-wrapper/MarkdownWrapper';
 import styles from './about.module.css';
-
-interface AboutProps {
-  content: string;
-}
 
 const About = () => {
     const content = getMarkdownContent('README.md');
@@ -17,14 +12,5 @@ const About = () => {
     </div>
   );
 };
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   const content = getMarkdownContent('README.md');
-//   return {
-//     props: {
-//       content,
-//     },
-//   };
-// };
 
 export default About;
