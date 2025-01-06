2b57059d7486113b177fdd73259b57cdb7267e0b
pa-dev <coderhookdev@gmail.com>
2025-01-06 13:38:24 +0100

    Update logger and gitignore with new changes

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,27 +1,51 @@
 import { createLogger, format, transports } from 'winston';
+import { TransformableInfo } from 'logform';
 
 const initializeLogger = () =>
   createLogger({
-    level: 'info',
+    level: process.env.LOG_LEVEL || 'info',
     format: format.combine(
       format.colorize(),
-      format.timestamp(),
-      format.printf((info: import('logform').TransformableInfo) => {
+      format.timestamp({
+        format: () => {
+          const date = new Date();
+          return date
+            .toLocaleString('en-GB', {
+              timeZone: 'Europe/Madrid', // Adjust to your local timezone
+              hour12: false,
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+            })
+            .replace(',', ''); // Format to local time in ISO-like format
+        },
+      }),
+      format.printf((info: TransformableInfo) => {
         const { timestamp, level, message } = info;
         return `${timestamp} ${level}: ${message}`;
       }),
     ),
     transports: [new transports.Console()],
   });
+const logger = initializeLogger();
 
 if (typeof window === 'undefined') {
   // Add file transport only on the server side
-  initializeLogger().add(
+  logger.add(
+    new transports.File({
+      filename: 'app-errors.log',
+      level: 'error',
+    }),
+  );
+
+  logger.add(
     new transports.File({
       filename: 'app.log',
     }),
   );
 }
 
-const logger = initializeLogger();
 export default logger;
