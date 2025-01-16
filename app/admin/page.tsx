975c622f46eaa8054e648464df619c7fd72b8b50
pa-dev <coderhookdev@gmail.com>
2025-01-16 21:49:56 +0100

    Suspense on LoginForm

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 import LoginForm from '@/components/login/LoginForm';
+import { Suspense } from 'react';
 
 const LoginPage = () => {
   return (
     <div className="flex items-center justify-center py-20 px-2">
       <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
