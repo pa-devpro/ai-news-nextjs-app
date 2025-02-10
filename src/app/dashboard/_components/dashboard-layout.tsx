72f4dcc3e8654927b57e1dc5461ca611f35f93cd
pa-dev <coderhookdev@gmail.com>
2025-02-10 23:46:10 +0100

    Add style on mobile and desktop for the dashboard-drawer menu

--- src/app/dashboard/_components/dashboard-layout.tsx
+++ src/app/dashboard/_components/dashboard-layout.tsx
@@ -21,6 +21,7 @@ import { paths } from '@/config/paths';
 import { cn } from '@/utils/cn';
 import { logout, useUserProfile } from '@/features/auth/utils/auth-utils';
 import { Spinner } from '@/components/dashboard/ui/spinner';
+import { useState } from 'react';
 
 type SideNavigationItem = {
   name: string;
@@ -32,6 +33,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const router = useRouter();
   const { data: user, isLoading } = useUserProfile();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   if (isLoading) return <Spinner />;
 
@@ -77,7 +79,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-60">
         <header className="sticky top-10 z-20 flex h-14 items-center justify-between gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:justify-end sm:border-0 sm:bg-transparent sm:px-6">
           {/* <Progress /> */}
-          <Drawer>
+          <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
             <DrawerTrigger asChild>
               <Button size="icon" variant="outline" className="sm:hidden">
                 <PanelLeft className="size-5" />
@@ -86,15 +88,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             </DrawerTrigger>
             <DrawerContent
               side="left"
-              className="bg-black pt-10 text-white sm:max-w-60"
+              className="bg-black pt-24 text-white sm:max-w-60 z-50 border-r-0" // Added border-r-0
             >
-              <nav className="grid gap-6 text-lg font-medium">
+              <nav className="grid gap-6 text-lg font-medium p-4">
                 {navigation.map((item) => {
                   const isActive = pathname === item.to;
                   return (
                     <NextLink
                       key={item.name}
                       href={item.to}
+                      onClick={() => setIsDrawerOpen(false)}
                       className={cn(
                         'text-gray-300 hover:bg-gray-700 hover:text-white',
                         'group flex flex-1 w-full items-center rounded-md p-2 text-base font-medium',
